8376da42c8da6674ced96f9f95eacac937340095
ivy799 <muhammadraihanaan123@gmail.com>
2025-01-27 17:59:36 +0800

    Refactor layout and authentication views with improved structure and styling

---
+++ database/migrations/2025_01_27_095102_create_category_news_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\News;
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_news', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(News::class);
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_news');
+    }
+};
