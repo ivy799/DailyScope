c03cdf99b48405725d793dd5223166134d888d15
ivy799 <muhammadraihanaan123@gmail.com>
2025-01-29 10:47:37 +0800

    Add Filament resources for News and Category management; implement search box functionality

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class News extends Model
 {
     /** @use HasFactory<\Database\Factories\NewsFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'image',
