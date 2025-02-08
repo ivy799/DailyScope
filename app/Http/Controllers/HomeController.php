7953d00fb81d83ebd32a653c21d5f0e27bfe2b7c
ivy799 <muhammadraihanaan123@gmail.com>
2025-02-08 20:22:16 +0800

    Implement caching for featured and latest news posts; optimize category retrieval in news index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\News;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -13,9 +14,18 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+
+        $featuredPosts = Cache::remember('featuredPosts', Carbon::now()->addHour(5), function(){
+            return News::published()->featured()->with('category')->latest('published_at')->take(3)->get();
+        });
+
+        $latestPosts = Cache::remember('latestPosts', Carbon::now()->addHour(5), function(){
+            return News::published()->with('category')->latest('published_at')->take(3)->get();
+        });
+
         return view('home', [
-            'featuredPosts' => News::published()->featured()->with('category')->latest('published_at')->take(3)->get(),
-            'latestPosts' => News::published()->with('category')->latest('published_at')->take(9)->get(),
+            'featuredPosts' => $featuredPosts,
+            'latestPosts' => $latestPosts,
         ]);
     }
 }
--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\News;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class NewsController extends Controller
 {
     public function index()
     {
 
+        $category = Cache::remember('category', Carbon::now()->addHour(5), function(){
+            return Category::whereHas('news', function($query){
+                $query->published();
+            })->take(10)->get();
+        });
+
         return view('news.index', [
-            'categories' => Category::whereHas('news')->take(10)->get()
+            'categories' => $category,
         ]);
     }
 
